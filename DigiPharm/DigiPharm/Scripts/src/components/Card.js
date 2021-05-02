83ecf93d7a9fcb78440a9e1c4fe8993aa40451df
Dibran Hoxha <dibran.ho18@gmail.com>
2021-05-02 23:08:13 +0200

    Integrated react inside asp.net mvc web application

---
+++ DigiPharm/DigiPharm/Scripts/src/components/Card.js
@@ -0,0 +1,20 @@
+﻿import React from "react";
+
+const Card = (props) => {
+	return (
+		<div className="card">
+			<img className="news-image" src={"~/images/logo192.png"} alt="test logo" />
+			<div className="card-body">
+				<h2>Title</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam.
+				</p>
+				<h5>Test author</h5>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
---
+++ DigiPharm/DigiPharm/Scripts/src/index.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './components/Card.js'
+
+const App = () => {
+	return (
+		<div>
+			<h1>New Asp.net + React Project</h1>
+			<Card />
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
